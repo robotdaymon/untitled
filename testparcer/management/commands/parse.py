293b7f2588301e939359430f0920319d95f368c5
shpion34rus <shpion34rus@ya.ru>
2019-10-15 15:32:14 +0400

    counting percentage

--- testparcer/management/commands/parse.py
+++ testparcer/management/commands/parse.py
@@ -7,13 +7,22 @@ class Command(BaseCommand):
     target_url = 'http://www.almhuette-raith.at/apache-log/access.log'
     data = urllib.request.urlopen(target_url)
     pattern = r'(\S+) (\S+) (\S+) \[(.*?)\] \"(\S+) (.*?) (\S+)\" (\S+) (\S+) (\".*?\") (\".*?\")'
+    total_length = data.length#кол-во байт в ответе
+    temp_percent = 0#вычисленный процент временный
+    temp_length = 0#кол-во байт в итерации
     count = 0
     for line in data:
         count += 1
-        if count % 1000 == 0:
-            print(count)
+        temp_length += len(line.decode('utf-8'))
+        current_percent = int((temp_length/total_length)*100)
+        if current_percent != temp_percent:
+            temp_percent = current_percent
+            print(str(current_percent) + '%')
+
+        if count % 10000 == 0:
+            # print(count)
             result = re.findall(pattern, str(line))
-            print(result)
+            # print(result)
     print(count)
 
     def reader(self):
