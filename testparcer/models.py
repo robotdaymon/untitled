0cb5d948bb7084ddaaa9fb83c4c7c836135a1f46
shpion34rus <shpion34rus@ya.ru>
2019-10-16 23:38:50 +0400

    model additional fields migration

--- testparcer/models.py
+++ testparcer/models.py
@@ -12,4 +12,5 @@ class Parceline(models.Model):
     urlrequest = models.TextField(blank=True)
     responsecode = models.CharField(max_length=20, blank=True)
     bytesread = models.BigIntegerField(default=0)
-
+    referrer = models.TextField(blank=True)
+    user_agent = models.TextField(blank=True)
