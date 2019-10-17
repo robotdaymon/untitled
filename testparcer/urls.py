ef0eae3ae7cb2b3ba2e211941fbef8898fb06adf
shpion34rus <shpion34rus@ya.ru>
2019-10-17 15:53:09 +0400

    add templates

--- testparcer/urls.py
+++ testparcer/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import *
 
 urlpatterns = [
     path('', index),
     path('index/', index),
+    path('common_table/', common_table),
+    path('login/', login)
 ]
--- testparcer/views.py
+++ testparcer/views.py
@@ -1,7 +1,22 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import AuthenticationForm
 
 
 def index(request):
-    return HttpResponse('<h1>Finally!</h1>')
+    return render(request, 'base.html')
+
+
+def common_table(request):
+    return HttpResponse('<h1>Finally!</h1><h1>Finally!</h1><h1>Finally!</h1><h1>Finally!</h1>')
+
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            return redirect('index/')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html' , {'form': form})
 
